8db062a4212486cc846de228bb940b9ff3306ce1
Asher Gilani <gilaniasher@gmail.com>
2021-10-29 14:40:35 -0400

    Routing isn't working right so just returning home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,13 @@
-import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom'
-
 import Home from './components/Home/Home'
-import SuccessCheckout from './components/SuccessCheckout'
-import ErrorCheckout from './components/ErrorCheckout'
+
+// import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+// import SuccessCheckout from './components/SuccessCheckout'
+// import ErrorCheckout from './components/ErrorCheckout'
 
 function App() {
   return (
-    <Router>
-      <div className='App'>
-        <ul>
-          <li><Link to='/'>Home!</Link></li>
-          <li><Link to='/success'>Success</Link></li>
-          <li><Link to='/error'>Error</Link></li>
-        </ul>
-      </div>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/success' component={SuccessCheckout} />
-        <Route exact path='/error' component={ErrorCheckout} />
-      </Switch>
-    </Router>
-  );
+    <Home />
+  )
 }
 
 export default App;
